7866e54986117109cb39c49a406b893636dd83b2
tomm3hgunn <57208697+tomm3hgunn@users.noreply.github.com>
2022-10-27 12:59:56 -0500

    tested adding API data to table via for loop

--- goforless/oddsAndEvents/views.py
+++ goforless/oddsAndEvents/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .models import *
+from .services import *
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "sportz/index.html")
+    json = oddSpreads()
+    variables = {"data": json}
+    return render(request, "sportz/index.html", variables)
