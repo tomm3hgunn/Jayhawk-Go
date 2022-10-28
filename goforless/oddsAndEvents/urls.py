3dc9b322c2d1480341c0c8891bf663f6ff492c87
tomm3hgunn <57208697+tomm3hgunn@users.noreply.github.com>
2022-10-27 19:44:03 -0500

    Made links to other tabs functional

--- goforless/oddsAndEvents/urls.py
+++ goforless/oddsAndEvents/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("news" , views.news, name="news"),
+    path("matches" , views.matches, name="matches"),
+    path("team", views.team, name="team"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
 ]
--- goforless/oddsAndEvents/views.py
+++ goforless/oddsAndEvents/views.py
@@ -10,3 +10,18 @@ def index(request):
     json = oddSpreads()
     variables = {"data": json}
     return render(request, "sportz/index.html", variables)
+
+def news(request):
+    return render(request, "sportz/news.html")
+
+def matches(request):
+    return render(request, "sportz/matches.html")
+
+def team(request):
+    return render(request, "sportz/team.html")
+
+def about(request):
+    return render(request, "sportz/about.html")
+
+def contact(request):
+    return render(request, "sportz/contact.html")
