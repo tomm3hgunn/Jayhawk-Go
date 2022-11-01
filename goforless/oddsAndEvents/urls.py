0b53b205e552a084b6ac7d63f4bf5ca9e5b0cf32
tomm3hgunn <57208697+tomm3hgunn@users.noreply.github.com>
2022-11-01 00:35:14 -0500

    changing landing page to "oddsAndEvents/index/"

--- goforless/oddsAndEvents/urls.py
+++ goforless/oddsAndEvents/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index", views.index, name="index"),
     path("news" , views.news, name="news"),
     path("matches" , views.matches, name="matches"),
     path("team", views.team, name="team"),
