af862ac4e86bfdd942e8b2833a307bfae161dd9f
tomm3hgunn <57208697+tomm3hgunn@users.noreply.github.com>
2022-11-12 15:10:05 -0600

    In apiDisplay, obtained all api data for total

--- goforless/apiDisplay/services.py
+++ goforless/apiDisplay/services.py
@@ -13,7 +13,7 @@ def oddSearch(region="us", markets="spreads"):
     baseUrl = f"https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds"
     response = requests.get(baseUrl, params=params)
     json = response.json()
-    pprint(json[0])
+    # pprint(json[0])
     return json
 
 def oddMoneyline():
@@ -22,6 +22,7 @@ def oddMoneyline():
 
 def oddTotals():
     json = oddSearch(markets="totals")
+    pprint(json[0])
     return json
 
 def oddOutrights():
