15ca57aa50aca93226f7f8bba406f6ac36713879
tomm3hgunn <57208697+tomm3hgunn@users.noreply.github.com>
2022-11-19 01:06:22 -0600

    issue #49: Every request from API is put into Spreads table in Django DB

--- goforless/oddsAndEvents/models.py
+++ goforless/oddsAndEvents/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Spreads(models.Model):
+    """Model of SQL table to hold Spreads data"""
+
+    homeTeam = models.CharField(max_length=200)
+    awayTeam = models.CharField(max_length=200)
+    bookmaker = models.CharField(max_length=200)
+    commenceTime = models.DateTimeField()
+    lastUpdated = models.DateTimeField()
+    gameID = models.CharField(max_length=200)
+    homePoints = models.FloatField()
+    homePrice = models.IntegerField()
+    awayPoints = models.FloatField()
+    awayPrice = models.IntegerField()
+
+    def __str__(self):
+        return  f"({self.bookmaker}) " + self.homeTeam + " vs. " + self.awayTeam + " " + self.lastUpdated.strftime("%m/%d/%Y, %H:%M:%S")
--- goforless/oddsAndEvents/services.py
+++ goforless/oddsAndEvents/services.py
@@ -3,7 +3,9 @@ import requests
 from dotenv import load_dotenv
 import os
 from pprint import pprint
-
+from oddsAndEvents.models import *
+import datetime
+import pytz
 
 def scoresSearch(daysFrom="3"):
     load_dotenv()
@@ -20,8 +22,13 @@ def oddSearch(region="us", markets="spreads"):
     load_dotenv()
     ODDS_API = os.environ["ODDS_API"]
 
-    params = {"regions": region, "markets": markets, "apiKey": ODDS_API,
-              "oddsFormat": "american", "bookmakers": "fanduel,barstool,williamhill_us,draftkings,betmgm"}
+    params = {
+        "regions": region,
+        "markets": markets,
+        "apiKey": ODDS_API,
+        "oddsFormat": "american",
+        "bookmakers": "fanduel,barstool,williamhill_us,draftkings,betmgm",
+    }
     baseUrl = f"https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds"
     response = requests.get(baseUrl, params=params)
     remainingReq = response.headers["X-Requests-Remaining"]
@@ -45,7 +52,48 @@ def oddTotals():
 def oddSpreads():
     print("requested spreads")
     json = oddSearch(markets="spreads")
+
+    # Every request, add data to database
+    for game in json:
+        awayTeam = game["away_team"]
+        homeTeam = game["home_team"]
+        commenceTime = game["commence_time"]
+        gameID = game["id"]
+        for bookmaker in game["bookmakers"]:
+            book = bookmaker["title"]
+            if book == "William Hill (US)":
+                book = "Caesars Sportsbook"
+            lastUpdate = bookmaker["last_update"]
+            if awayTeam == bookmaker["markets"][0]["outcomes"][0]:
+                awayPoints = bookmaker["markets"][0]["outcomes"][0]["point"]
+                awayPrice = bookmaker["markets"][0]["outcomes"][0]["price"]
+                homePoints = bookmaker["markets"][0]["outcomes"][1]["point"]
+                homePrice = bookmaker["markets"][0]["outcomes"][1]["price"]
+            else:
+                awayPoints = bookmaker["markets"][0]["outcomes"][1]["point"]
+                awayPrice = bookmaker["markets"][0]["outcomes"][1]["price"]
+                homePoints = bookmaker["markets"][0]["outcomes"][0]["point"]
+                homePrice = bookmaker["markets"][0]["outcomes"][0]["price"]
+            cursor = Spreads(
+                homeTeam=homeTeam,
+                awayTeam=awayTeam,
+                bookmaker=book,
+                commenceTime=commenceTime,
+                lastUpdated=lastUpdate,
+                gameID=gameID,
+                homePoints=homePoints,
+                homePrice=homePrice,
+                awayPoints=awayPoints,
+                awayPrice=awayPrice,
+            )
+            cursor.save()
+        # print number of rows in database
+    print("Number of rows in Spreads Table: " + str(Spreads.objects.count()))
+        
+    # cursor = Spreads(homeTeam="test", awayTeam="test", bookmaker="test", commenceTime="test", lastUpdated="test", gameID="test", homePoints="test", homePrice="test", awayPoints="test", awayPrice="test")
+    # cursor.save()
     return json
+
 
 # not sure yet where these func needs to be this can be temp
 # lowest to highest might be they best display
